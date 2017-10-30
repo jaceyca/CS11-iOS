f214dd8d194a13892fd9abab7240e2e04eb3ce36
jaceyca <choi.jess816@gmail.com>
2017-10-29 22:55:10 -0700

    Beginning implementation of geocache app

---
+++ Lab3/Geocache.swift
@@ -0,0 +1,73 @@
+//
+//  Geocache.swift
+//  Lab3
+//
+//  Created by Jessica Choi on 10/29/17.
+//  Copyright © 2017 Jessica Choi. All rights reserved.
+//
+
+import Foundation
+
+struct GeoCache {
+    var title: String
+    var details: String
+    var creator: String
+    var reward: String
+    
+    init?(fromDictionary dict: [String: String]) {
+        if let title = dict["title"] {
+            self.title = title
+        } else {
+            return nil
+        }
+        if let details = dict["details"] {
+            self.details = details
+        } else {
+            return nil
+        }
+        if let creator = dict["creator"] {
+            self.creator = creator
+        } else {
+            return nil
+        }
+        if let reward = dict["reward"] {
+            self.reward = reward
+        } else {
+            return nil
+        }
+    }
+    
+    var dictionary : [String: String] {
+        let dict: [String: String] = ["title": title, "details": details, "creator": creator, "reward": reward]
+        return dict
+    }
+    
+    var description : String {
+        let desc: String = "Title: \(title), details: \(details), creator: \(creator), reward: \(reward)"
+        return desc
+    }
+}
+
+func loadCachesFromDefaults() -> [GeoCache] {
+    let defaults = UserDefaults.standard.array(forKey: "caches")
+    var geocaches: [GeoCache] = []
+    for i in defaults! {
+        if let geo = i as? [String: String] {
+            geocaches.append(GeoCache(fromDictionary: geo)!)
+        }
+    }
+    return geocaches
+}
+
+func saveCachesToDefaults(_ caches: [GeoCache]) {
+    UserDefaults.standard.set(caches, forKey:"caches")
+    UserDefaults.standard.synchronize()
+}
+
+
+
+
+
+
+
+
--- Lab3/ViewController.swift
+++ Lab3/ViewController.swift
@@ -10,6 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var detailField: UITextField!
+    @IBOutlet weak var creatorField: UITextField!
+    @IBOutlet weak var rewardField: UITextField!
+    @IBOutlet weak var cacheLabel: UITextView!
+    
+    var counter = 0
+    
+    @IBAction func addGeoCache(_ sender: UIButton) {
+        let title = titleField.text
+        let details = detailField.text
+        let creator = creatorField.text
+        let reward = rewardField.text
+        
+        if titleField.text == "" || detailField.text == "" || creatorField.text == "" || rewardField.text == "" {
+            cacheLabel.text = "All fields need to be filled."
+        } else {
+            let dict: [String: String] = ["title": title!, "details": details!, "creator": creator!, "reward": reward!]
+            let geo = GeoCache(fromDictionary: dict)
+            var geocaches = loadCachesFromDefaults()
+            geocaches.append(geo!)
+            saveCachesToDefaults(geocaches)
+        }
+    }
+    
+    @IBAction func nextCache(_ sender: UIButton) {
+        var Geocaches: [GeoCache] = loadCachesFromDefaults()
+        counter += 1
+        if counter >= Geocaches.count {
+            counter = 0
+        }
+        let geocache = Geocaches[counter]
+        cacheLabel.text = geocache.description
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
